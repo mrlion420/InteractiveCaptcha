795313854e6804892fb584846129f77e8c6c53c2
mrlion420 <mrlion420@gmail.com>
2018-03-24 14:17:37 +0800

    Sample code for xiaolan

--- InteractiveCaptcha WebSite/js/script.js
+++ InteractiveCaptcha WebSite/js/script.js
@@ -1,1 +1,12 @@
-﻿
+﻿function getCaptchaImages(){
+    // Get image
+    let data = [];
+    let htmlString ="";
+    for(let i = 0; i < 9; i++){
+        htmlString += "<div id='tile" + i + "'>";
+        htmlString += "<img src='" + data[i] +"' class='bigger'/>";
+        htmlString += "</div>";
+    }
+
+    $("#container").html(htmlString);
+}
