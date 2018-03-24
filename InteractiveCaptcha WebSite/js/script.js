d85c0b3546cf8311e63f4454b144b01b621d6335
Xiaolan <xcai017@mymail.sim.edu.sg>
2018-03-24 12:45:51 +0800

    Added Rotate image and hover

--- InteractiveCaptcha WebSite/js/script.js
+++ InteractiveCaptcha WebSite/js/script.js
@@ -1,1 +1,20 @@
-﻿
+﻿var angle = 0;
+
+$(document).ready(function () {
+    imageClick();
+});
+
+function imageClick() {
+
+    //var angle = 0,
+    //    img = document.getElementById('container');
+    //document.getElementById('image').onclick = function () {
+    //    angle = (angle + 90) % 360;
+    //    img.className = "rotate" + angle;
+    //}
+    $("#image").click(function () {
+        angle += 90;
+        $("#image").css("transform", "rotate(" + angle + "deg)");
+    })
+}
+
