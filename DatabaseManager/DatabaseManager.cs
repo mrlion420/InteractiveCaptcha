306eb6aa2c39c1efabff46e9381cc8d7fcbcc93d
mrlion420 <mrlion420@gmail.com>
2018-05-20 05:55:51 +0000

    Changed back to localhost database

--- DatabaseManager/DatabaseManager.cs
+++ DatabaseManager/DatabaseManager.cs
@@ -26,7 +26,7 @@ namespace DatabaseManager
         }
         private void Initialize()
         {
-            server = "http://122.11.177.14:";
+            server = "localhost";
             database = "interactive_captcha";
             uid = "root";
             password = "toor";
