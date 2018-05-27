ed47a48cb695c6985b28c7122362cdf5b37cab78
Xiaolan <xcai017@mymail.sim.edu.sg>
2018-05-27 16:08:53 +0800

    Add test script demos

--- InteractiveCaptcha WebSite/js/testscript.js
+++ InteractiveCaptcha WebSite/js/testscript.js
@@ -72,22 +72,22 @@
 
 //TEST ShowSuccessMesg -------------
 
-$(document).ready(function () {
-    test_Init();
-    test_ShowSuccessMesg();
-});
+//$(document).ready(function () {
+//    test_Init();
+//    test_ShowSuccessMesg();
+//});
 
-function test_Init() {
-    interactive_captcha.Init("testContainer", callback_One);
-}
+//function test_Init() {
+//    interactive_captcha.Init("testContainer", callback_One);
+//}
 
-function callback_One(data) {
+//function callback_One(data) {
 
-}
+//}
 
-function test_ShowSuccessMesg() {
-    interactive_captcha.ShowSuccessMesg("Successful!");
-}
+//function test_ShowSuccessMesg() {
+//    interactive_captcha.ShowSuccessMesg("Successful!");
+//}
 
 
 //////TEST Reload --------------------------------
@@ -116,12 +116,100 @@ function test_ShowSuccessMesg() {
 //}
 
 
+//TEST Config 1
+
+//$(document).ready(function () {
+//    test_Init();
+//    test_Config1();
+//});
+
+//function test_Init() {
+//    interactive_captcha.Init("testContainer", callback_One);
+//}
+
+//function callback_One(data) {
+
+//}
+
+//function test_Config1() {
+//    let data = {
+//        ColorConfirmBtn: "black",
+//        ColorReloadBtn: "black",
+//        ColorConfirmBtnTxt: "red",
+//        ColorReloadBtnTxt: "red",
+//        ColorConfirmBtnBorder: "yellow",
+//        ColorReloadBtnBorder: "yellow",
+//        ColorCaptchaBackground: "lightgrey",
+//        ColorCaptchaTitle: "red",
+//        ShowCaptchaTitle: false,
+//        ConfirmBtnBorder: "4px solid yellow",
+//        ReloadBtnBorder: "4px solid",
+//        OuterBoxShadow: "2px 2px 2px 2px blue"
+//    };
+//    interactive_captcha.Config(data);
+//}
+
+//TEST Config 2
+
+//$(document).ready(function () {
+//    test_Init();
+//    test_Config2();
+//});
+
+//function test_Init() {
+//    interactive_captcha.Init("testContainer", callback_One);
+//}
+
+//function callback_One(data) {
+
+//}
+
+//function test_Config2() {
+//    let data = {
+//        ColorConfirmBtn: "green",
+//        ColorReloadBtn: "green",
+//        ColorConfirmBtnTxt: "white",
+//        ColorReloadBtnTxt: "white",
+//        ColorConfirmBtnBorder: "red",
+//        ColorReloadBtnBorder: "red",
+//        ColorCaptchaBackground: "white",
+//        ColorCaptchaTitle: "black",
+//        ShowCaptchaTitle: false,
+//        ConfirmBtnBorder: "2px solid white",
+//        ReloadBtnBorder: "2px solid white",
+//        OuterBoxShadow: "2px 2px 2px 2px white"
+//    };
+//    interactive_captcha.Config(data);
+//}
 
 
-// =====================
+//TEST setcurrentlocale and addnewlocale
 
-//interactive_captcha.SetCallback(callback_Two);
+//$(document).ready(function () {
+//    test_init();
+//    test_addnewlocale();
+//});
 
-//function callback_Two(data) {
+//function test_init() {
+//    interactive_captcha.Init("testContainer", callback_one);
+//}
 
+//function callback_one(data) {
+
+//}
+
+//function test_addnewlocale() {
+//    let newlocale = {
+//        "zh": {
+//            chartTitle: "请点击旋转图片",
+//            confirmBtnTxt: "确定",
+//            reloadBtnTxt: "刷新",
+//            successTxt: "验证成功！",
+//            errorTxt: "验证错误，请重试！"
+//        }
+
+//    };
+
+//    interactive_captcha.AddNewLocale(newlocale);
+//    interactive_captcha.SetCurrentLocale("zh", true);
 //}
